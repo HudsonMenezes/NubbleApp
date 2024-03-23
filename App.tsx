2a7d76e39f75b79efbb2a5f85ddfb5c7bf8e127d
Hudson Menezes <husdon.menezes@criaresistemas.com>
2024-03-23 18:00:42 -0300

    fontFamily config done

--- App.tsx
+++ App.tsx
@@ -5,7 +5,12 @@ import {Text} from './src/components/Text/Text';
 function App(): JSX.Element {
   return (
     <SafeAreaView>
-      <Text preset="headingLarge">Hello, world!</Text>
+      <Text preset="headingLarge" style={{color: '#000'}}>
+        Hello, world!
+      </Text>
+      <Text preset="headingLarge" style={{color: '#000'}}>
+        Hello, world! 2
+      </Text>
     </SafeAreaView>
   );
 }
