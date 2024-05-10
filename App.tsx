ec25b3ea13937a9b5f73bc993f71c401c6f21708
Hudson Menezes <husdon.menezes@criaresistemas.com>
2024-05-10 20:39:09 -0300

    textInput component done

--- App.tsx
+++ App.tsx
@@ -8,29 +8,40 @@ import {theme} from './src/theme/theme';
 import {Box} from './src/components/Box/Box';
 import {Button} from './src/components/Button/Button';
 import {TextInput} from './src/components/TextInput/TextInput';
+import {Icon} from './src/components/Icon/Icon';
 
 function App(): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaView>
         <View style={{paddingHorizontal: 24}}>
-          <Text preset="headingLarge" mb="s8">
+          <Text marginBottom="s8" preset="headingLarge">
             Olá
           </Text>
           <Text preset="paragraphLarge" mb="s40">
             Digite seu e-mail e senha para entrar
           </Text>
 
-          <TextInput label="E-mail" />
+          <TextInput
+            errorMessage="mensagem de error"
+            label="E-mail"
+            placeholder="Digite seu e-mail"
+            boxProps={{mb: 's20'}}
+          />
 
-          <TextInput label="Senha" />
+          <TextInput
+            label="Senha"
+            placeholder="Digite sua senha"
+            RightComponent={<Icon color="gray2" name="eyeOn" />}
+            boxProps={{mb: 's10'}}
+          />
 
-          <Text color="primary" preset="paragraphSmall" bold mt="s10">
+          <Text color="primary" preset="paragraphSmall" bold>
             Esqueci minha senha
           </Text>
 
-          <Button title="Entrar" mt="s48" />
-          <Button title="Criar uma conta" mt="s12" preset="outline" />
+          <Button marginTop="s48" title="Entrar" />
+          <Button preset="outline" marginTop="s12" title="Criar uma conta" />
         </View>
       </SafeAreaView>
     </ThemeProvider>
