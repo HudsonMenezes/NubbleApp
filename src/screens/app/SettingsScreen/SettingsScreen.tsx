ea3393ad82705e6685c7c7b57b63f0dbb45078e1
Hudson Menezes <husdon.menezes@criaresistemas.com>
2024-09-02 19:43:25 -0300

    refatoração AuthScreen

--- src/screens/app/SettingsScreen/SettingsScreen.tsx
+++ src/screens/app/SettingsScreen/SettingsScreen.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import {Screen, Text} from '@components';
+import {AppScreenProps} from '@routes';
 
-export function SettingsScreen() {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function SettingsScreen(Props: AppScreenProps<'SettingsScreen'>) {
   return (
-    <Screen>
+    <Screen canGoBack>
       <Text preset="headingSmall">Settings Screen</Text>
     </Screen>
   );
