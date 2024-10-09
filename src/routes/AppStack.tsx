ce816bfc91435344ee8d13ecc89dc599b8b43851
Hudson Menezes <husdon.menezes@criaresistemas.com>
2024-10-09 15:53:43 -0300

    finalizado módulo 11 - navegação avançada

--- src/routes/AppStack.tsx
+++ src/routes/AppStack.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import {NavigatorScreenParams} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import {HomeScreen, SettingsScreen} from '@screens';
+import {SettingsScreen} from '@screens';
+
+import {AppTabBottomTabParamList, AppTabNavigator} from './AppTabNavigator';
 
 export type AppStackParamList = {
-  HomeScreen: undefined;
+  AppTabNavigator: NavigatorScreenParams<AppTabBottomTabParamList>;
   SettingsScreen: undefined;
 };
 
@@ -17,8 +20,9 @@ export function AppStack() {
       screenOptions={{
         headerShown: false,
         fullScreenGestureEnabled: true,
-      }}>
-      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      }}
+      initialRouteName="AppTabNavigator">
+      <Stack.Screen name="AppTabNavigator" component={AppTabNavigator} />
       <Stack.Screen name="SettingsScreen" component={SettingsScreen} />
     </Stack.Navigator>
   );
