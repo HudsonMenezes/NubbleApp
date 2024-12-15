d02f54971bba8f9b4fa9ab780e99e37885a97cd7
Hudson Menezes <husdon.menezes@criaresistemas.com>
2024-12-15 16:07:30 -0300

    Inserido tipos no postAPI

--- src/domain/Post/postApi.ts
+++ src/domain/Post/postApi.ts
@@ -1,10 +1,22 @@
-import {postListMock} from './postListMock';
-import {Post} from './types';
+import {PageAPI} from '@api';
 
-async function getList(): Promise<Post[]> {
-  //TODO: simular um delay na API
-  await new Promise(resolve => setTimeout(() => resolve(''), 1000));
-  return postListMock;
+import {PostAPI} from './postTypes';
+
+async function getList(): Promise<PageAPI<PostAPI>> {
+  let response = await fetch('http://127.0.0.1:3333/user/post', {
+    method: 'GET',
+    headers: {
+      Authorization:
+        'Bearer Mg.xWl7KDhBa5KL-9OxRkyYShJnZCAIhJ5rSyU2feCQqsYhbhgbWyCaTtyQvLvu',
+    },
+  });
+
+  let data: PageAPI<PostAPI> = await response.json();
+
+  return data;
+
+  // await new Promise(resolve => setTimeout(() => resolve(''), 1000));
+  // return postListMock;
 }
 
 export const postApi = {
